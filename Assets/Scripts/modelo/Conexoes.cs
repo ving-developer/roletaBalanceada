2daf0a9669ebff7da3a942545a1145476261664b
luizloja <luizloja@gmail.com>
2020-02-07 14:35:52 -0300

    Plugins, Modelo e Banco

---
+++ Assets/Scripts/modelo/Conexoes.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Data;
+using Mono.Data.SqliteClient;
+
+public class Conexoes {
+    private static string urlDataBase = "URI=file:bancoQuimico.db";
+    private static IDbConnection _connection = null;
+
+    public static IDataReader pegarReader(string query) {
+        _connection = new SqliteConnection(urlDataBase);
+
+        IDbCommand _command = _connection.CreateCommand();
+        _connection.Open();
+
+        _command.CommandText = query;
+
+        IDataReader reader = _command.ExecuteReader();
+        return reader;
+
+    }
+
+    public static void fecharConexao() {
+        _connection.Close();
+    }
+
+}
--- Assets/Scripts/modelo/Jogador.cs
+++ Assets/Scripts/modelo/Jogador.cs
@@ -1,59 +1,16 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;	
 
 
 public class Jogador{
-	private String nome;
+	
+	
+	private string nome;
+
 	private int pontuacao;
-	private float tempo;
-	
-	public Jogador(int pontuacao, int tempo)
-	{
-		this.pontuacao = pontuacao;
-		this.tempo = tempo;
-	}
-	
-	public String getTempo()
-	{
-		int horas = (int) tempo / 3600;
-		int minutos = (int)(tempo % 3600) / 60;
-		int segundos = (int)((tempo % 3600) %60);
-		return horas.ToString() + ":" + minutos.ToString("D2") + ":" + segundos.ToString("D2");
-	}
-	
-	public String addTempo(float segundos)
-	{
-		tempo += segundos;
-		return getTempo();
-	}
 
-	public String setTempo(float segundos)
-	{
-		tempo = (int) segundos;
-		return getTempo();
-	}
-	
-	public String getPontuacao()
-	{
-		return pontuacao.ToString();
-	}
+	private Time tempo;
 
-	public void addPontuacao(int quantidade)
-	{
-		pontuacao += quantidade;
-	}
-
-
-	public void setNome(String nome)
-	{
-		this.nome = nome;
-	}
-
-	public String getNome()
-	{
-		return nome;
-	}
 }
 
