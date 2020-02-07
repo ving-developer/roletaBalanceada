a19992eba91ca52f12d26211525f9627ad123e5d
Henrique Barros <henrique.sb0709@gmail.com>
2020-02-07 14:28:20 -0300

    Revert "Modelo"
    This reverts commit d666305c29e58003948a8a4f5c4fb606c7109070.

--- Assets/Scripts/modelo/Equacao.cs
+++ Assets/Scripts/modelo/Equacao.cs
@@ -1,86 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-using System.Data;
-using Mono.Data.SqliteClient;
+using UnityEngine;	
+
 
 
 public class Equacao{
-
-    int id;
-    /// Fácil
+	/// Fácil
 	/// Médio
 	/// Difícil
-	private string tipo;
+	private Enum tipo;
 
 	private Time tempoMedioResposta;
 
 	private Time melhorTempoResposta;
 
-	private List<MoleculaForma> reagente = new List<MoleculaForma>();
+	private MoleculaForma reagente;
 	
-	private List<MoleculaForma> produto = new List<MoleculaForma>();
-
-    private static Dictionary<int, Equacao> equacoes;
-
-    public string Tipo { get => tipo; set => tipo = value; }
-    public Time TempoMedioResposta { get => tempoMedioResposta; set => tempoMedioResposta = value; }
-    public Time MelhorTempoResposta { get => melhorTempoResposta; set => melhorTempoResposta = value; }
-    public int Id { get => id; set => id = value; }
-    public List<MoleculaForma> Reagente { get => reagente; set => reagente = value; }
-    public List<MoleculaForma> Produto { get => produto; set => produto = value; }
-
-    public static Dictionary<int, Equacao> todasEquacoes() {
-
-        if (equacoes == null){
-            equacoes = new Dictionary<int, Equacao>();
-
-            IDataReader reader = Conexoes.pegarReader("SELECT id, tipo FROM equacao");
-            while (reader.Read())
-            {
-                Equacao equacao = new Equacao();
-                equacao.Id = reader.GetInt32(0);
-                equacao.Tipo = reader.GetString(1);
-                equacoes[equacao.Id] = equacao;
-            }
-            Conexoes.fecharConexao();
-
-        }
-
-        foreach (int index in equacoes.Keys){
-            MoleculaForma.carregarMoleculaForma(equacoes[index]);
-        }
+	private MoleculaForma produto;
 
 
+	/// Verifica se o número de atomos de um lado é igual ao número de atomos do outro lado
+	public void verificarResposta()
+	{
 
-        return equacoes;
-    }
-
-    override
-    public string ToString() {
-
-        string equacaoReagente = "";
-
-        foreach(MoleculaForma reagente in Reagente){
-            if(equacaoReagente == ""){
-                equacaoReagente += $"{reagente.Balanco}{reagente.Molecula}";
-            } else{
-                equacaoReagente += $" + {reagente.Balanco}{reagente.Molecula}";
-            }
-
-        }
-
-        string equacaoProduto = "";
-        foreach (MoleculaForma produto in Produto){
-            if (equacaoProduto == "" ){
-                equacaoProduto += $"{produto.Balanco}{produto.Molecula}";
-            } else{
-                equacaoProduto += $" + {produto.Balanco}{produto.Molecula}";
-            }
-        }
-
-        return $"{equacaoReagente} = {equacaoProduto}";
-    }
+	}
 
 }
 
--- Assets/Scripts/modelo/Molecula.cs
+++ Assets/Scripts/modelo/Molecula.cs
@@ -1,68 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-using System.Data;
-using Mono.Data.SqliteClient;
+using UnityEngine;	
 
 public class Molecula
 {
-    private int id;
-	private string nome;
+	private String nome;
 
-    private static Dictionary<int, Molecula> moleculas;
-
-    private List<AtomoQuantificado> atomosQuantificados;
+	private AtomoQuantificado[] atomoQuantificado;
 	
-	private List<MoleculaCompoe> moleculasCompoePrimaria = new List<MoleculaCompoe>();
+	private MoleculaCompoe[] moleculaCompoe;
 
-    public string Nome { get => nome; set => nome = value; }
-    public int Id { get => id; set => id = value; }
-    public List<AtomoQuantificado> AtomosQuantificados { get => atomosQuantificados; set => atomosQuantificados = value; }
-    public List<MoleculaCompoe> MoleculasCompoePrimaria { get => moleculasCompoePrimaria; set => moleculasCompoePrimaria = value; }
-
-    public static Dictionary<int, Molecula> todasMoleculas() {
-        if (moleculas == null)
-        {
-            moleculas = new Dictionary<int, Molecula>();
-
-            IDataReader reader = Conexoes.pegarReader("SELECT id, nome FROM molecula");
-            while (reader.Read())
-            {
-                Molecula molecula = new Molecula();
-                molecula.Id = reader.GetInt32(0);
-                molecula.Nome = reader.GetString(1);
-                moleculas[molecula.Id] = molecula;
-            }
-            Conexoes.fecharConexao();
-
-            foreach (int index in moleculas.Keys){
-                moleculas[index].AtomosQuantificados = AtomoQuantificado.carregar(moleculas[index]);
-            }
-
-        }
-
-        MoleculaCompoe.carregarComposicao(moleculas);
-
-
-
-        return moleculas;
-    }
-
-    override
-    public string ToString() {
-        string nomeMolecula = "";
-
-        for(int i=0,seqAtomo=0, seqMolecula =0; i < AtomosQuantificados.Count + MoleculasCompoePrimaria.Count;i++){
-            if(AtomosQuantificados.Count > seqAtomo &&   i == AtomosQuantificados[seqAtomo].Sequencia){
-                nomeMolecula += AtomosQuantificados[seqAtomo].ToString();
-                seqAtomo++;
-            } else if (MoleculasCompoePrimaria.Count > seqMolecula && i == MoleculasCompoePrimaria[seqMolecula].Sequencia){
-                nomeMolecula += this.moleculasCompoePrimaria[seqMolecula].ToString();
-                seqMolecula++;
-            }
-        }
-        return nomeMolecula;
-    }
+	public String moleculaPorExtenso()
+	{
+		return null;
+	}
 
 }
 
