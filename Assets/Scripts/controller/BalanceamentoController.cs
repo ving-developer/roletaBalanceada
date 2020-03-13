3de550408f95a05f132a88da4d8148ad4f582e1d
luizloja <luizloja@gmail.com>
2020-03-13 20:02:48 -0300

    COmponente de slider e roleta

--- Assets/Scripts/controller/BalanceamentoController.cs
+++ Assets/Scripts/controller/BalanceamentoController.cs
@@ -7,6 +7,9 @@ public class BalanceamentoController : MonoBehaviour
     public AudioSource errou;
     public AudioSource acertou;
     public AudioSource backgroundSound;
+    public SpriteRenderer estrelaEsquerda;
+    public SpriteRenderer estrelaMeio;
+    public SpriteRenderer estrelaDireita;
 
     public Text tituloResposta;
     public GameObject ResultScreen;
@@ -17,6 +20,7 @@ public class BalanceamentoController : MonoBehaviour
     public void Start() {
         tempoJogada = 0; 
         playSound();
+        acenderEstrelas();
     }
 
     private void Update(){
@@ -35,18 +39,57 @@ public class BalanceamentoController : MonoBehaviour
         ResultScreen.SetActive(true);
         
         if (verifyResult()){
-            Debug.Log("Cheguei 2.5");
             ResultScreen.GetComponent<ResultScreen>().showBlast();
-            Debug.Log($"Tempo: {tempoJogada} porcentagem: {(((int)tempoJogada) * 100 / NucleoController.preferences["TempoConfiguracao"].getInt())} ");
-            nucleo.jogadores[nucleo.jogada].addPontuacao( (nucleo.pontuacaoAcerto * (100 - (((int)tempoJogada) * 100/NucleoController.preferences["TempoConfiguracao"].getInt()))/100 ) + 5  );
+            Debug.Log(tempoJogada);
+            Debug.Log(NucleoController.preferences["TempoConfiguracao"].getInt());
+            Debug.Log(((((int)tempoJogada) * 100 / NucleoController.preferences["TempoConfiguracao"].getInt())) );
+            int valorPercentual = 100 - (( ((int)tempoJogada) * 100 / NucleoController.preferences["TempoConfiguracao"].getInt())) ;
+            Debug.Log(valorPercentual);
+            int valorPontuado = (nucleo.pontuacaoAcerto * valorPercentual/100 ) + 5;
+            arrumarEstrelas(valorPercentual);
+            nucleo.jogadores[nucleo.jogada].addPontuacao(valorPontuado);
             tituloResposta.text = "Parabéns!";
             acertou.Play();
         } else{
+            apagarEstrelas();
             errou.Play();
             tituloResposta.text = "Errou!";
             nucleo.jogadores[nucleo.jogada].addPontuacao(nucleo.pontuacaoErro);
         }
     }
+
+
+    private void arrumarEstrelas(int valorPercentual) {
+        if (valorPercentual< 25){
+            apagarEstrelas();
+        } else if (valorPercentual < 50){
+            apagarDuas();
+        } else if (valorPercentual < 75){
+            apagarUma();
+        } 
+    }
+
+    private void apagarEstrelas() {
+        estrelaDireita.GetComponent<EstrelaControlador>().apagar();
+        estrelaEsquerda.GetComponent<EstrelaControlador>().apagar();
+        estrelaMeio.GetComponent<EstrelaControlador>().apagar();
+    }
+
+    private void acenderEstrelas() {
+        estrelaDireita.GetComponent<EstrelaControlador>().acender();
+        estrelaEsquerda.GetComponent<EstrelaControlador>().acender();
+        estrelaMeio.GetComponent<EstrelaControlador>().acender();
+    }
+
+    private void apagarUma() {
+        estrelaDireita.GetComponent<EstrelaControlador>().apagar();
+    }
+
+    private void apagarDuas() {
+        estrelaEsquerda.GetComponent<EstrelaControlador>().apagar();
+        estrelaDireita.GetComponent<EstrelaControlador>().apagar();
+    }
+
 
     private bool verifyResult()
     {
--- Assets/Scripts/view/Formule.cs
+++ Assets/Scripts/view/Formule.cs
@@ -10,18 +10,23 @@ public class Formule : MonoBehaviour
     public Text formuleQuanity;
     public Text formuleName;
     public Slider sliderValor;
+    public Text valorSliderAcima;
     private int tipo;//0 para produto 1 para reagente
     
     // Start is called before the first frame update
     void Start()
     {
         formuleQuanity.text = "0";
-    }
+            }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+     void mostrarValor(PointerEventData data) {
+
     }
 
     public void more()
@@ -35,11 +40,10 @@ public class Formule : MonoBehaviour
 
     public void less()
     {
-        int value = int.Parse(formuleQuanity.text);
-        if(value>0)
+        int value = getQuanity();
+        if (value > 0)
         {
-            formuleQuanity.text = (value -= 1).ToString();
-            MoleculeQuanityAdapter.atualizarBalanco(formuleName.text,tipo,value);
+            sliderValor.value = sliderValor.value - 1;
         }
     }
 
